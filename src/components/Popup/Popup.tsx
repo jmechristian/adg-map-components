d8f9189ede1b65da3ef7cb14861dcb4b721f7845
JMEChristian Digital <jmechristian@gmail.com>
2022-06-29 16:43:35 -0400

    update to popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,11 +1,94 @@
 import React from 'react';
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  HStack,
+  Tag,
+  TagLabel,
+  Image,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 export interface PopupProps {
-  label: string;
+  name: string;
+  hero?: string;
+  department: string;
+  subcategory?: string;
+  building_type?: string;
+  link?: string;
+  description?: string;
 }
 
-const Popup = (props: PopupProps) => {
-  return <div>{props.label}</div>;
+const Popup = ({
+  name,
+  hero,
+  department,
+  subcategory,
+  building_type,
+  link,
+  description,
+}: PopupProps) => {
+  return (
+    <Box bg='white' width={'250px'}>
+      <Flex direction={'column'} padding='6px'>
+        {hero ? (
+          <Box marginBottom={'10px'}>
+            <Image
+              boxSize={'100%'}
+              src={`${hero}`}
+              alt={name ? name : 'Akseizer Design Group'}
+            />
+          </Box>
+        ) : (
+          ''
+        )}
+        <Flex>
+          <Text color={'gray.500'} fontSize={'xs'} fontStyle={'italic'}>
+            {department ? department : ''}
+            {subcategory ? ', ' + subcategory : ''}
+            {building_type ? ', ' + building_type : ''}
+          </Text>
+        </Flex>
+        <Box>
+          <Heading as='h4' size={'md'}>
+            {name}
+          </Heading>
+        </Box>
+        <Box marginBottom={'20px'}>
+          <Text noOfLines={3} lineHeight='short'>
+            {description ? description : ''}
+          </Text>
+        </Box>
+        <Box>
+          {link ? (
+            <HStack spacing={'4'}>
+              <Tag
+                size={'md'}
+                key={'md'}
+                variant='solid'
+                color={'white'}
+                bg={'blackAlpha.800'}
+                py={'6px'}
+                px={'12px'}
+                borderRadius='4px'
+                css={{ cursor: 'pointer' }}
+              >
+                <TagLabel fontSize={'sm'}>
+                  <a href={`${link}`} target='_blank' rel='noreferrer'>
+                    View Project
+                  </a>
+                </TagLabel>
+              </Tag>
+            </HStack>
+          ) : (
+            ''
+          )}
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Popup;
