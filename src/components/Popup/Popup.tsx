7a2e2c3eb8b94f8763787d0a0b14e1f446aa6a97
JMEChristian Digital <jmechristian@gmail.com>
2022-06-29 18:07:38 -0400

    sdsdsd

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -8,6 +8,7 @@ import {
   Tag,
   TagLabel,
   Image,
+  ChakraProvider,
 } from '@chakra-ui/react';
 
 export interface PopupProps {
@@ -30,63 +31,65 @@ function Popup({
   description,
 }: PopupProps) {
   return (
-    <Box bg='white' width={'250px'}>
-      <Flex direction={'column'} padding='6px'>
-        {hero ? (
-          <Box marginBottom={'10px'}>
-            <Image
-              boxSize={'100%'}
-              src={`${hero}`}
-              alt={name ? name : 'Akseizer Design Group'}
-            />
-          </Box>
-        ) : (
-          ''
-        )}
-        <Flex>
-          <Text color={'gray.500'} fontSize={'xs'} fontStyle={'italic'}>
-            {department ? department : ''}
-            {subcategory ? ', ' + subcategory : ''}
-            {building_type ? ', ' + building_type : ''}
-          </Text>
-        </Flex>
-        <Box>
-          <Heading as='h4' size={'md'}>
-            {name}
-          </Heading>
-        </Box>
-        <Box marginBottom={'20px'}>
-          <Text noOfLines={3} lineHeight='short'>
-            {description ? description : ''}
-          </Text>
-        </Box>
-        <Box>
-          {link ? (
-            <HStack spacing={'4'}>
-              <Tag
-                size={'md'}
-                key={'md'}
-                variant='solid'
-                color={'white'}
-                bg={'blackAlpha.800'}
-                py={'6px'}
-                px={'12px'}
-                borderRadius='4px'
-                css={{ cursor: 'pointer' }}
-              >
-                <TagLabel fontSize={'sm'}>
-                  <a href={`${link}`} target='_blank' rel='noreferrer'>
-                    View Project
-                  </a>
-                </TagLabel>
-              </Tag>
-            </HStack>
+    <ChakraProvider>
+      <Box bg='white' width={'250px'}>
+        <Flex direction={'column'} padding='6px'>
+          {hero ? (
+            <Box marginBottom={'10px'}>
+              <Image
+                boxSize={'100%'}
+                src={`${hero}`}
+                alt={name ? name : 'Akseizer Design Group'}
+              />
+            </Box>
           ) : (
             ''
           )}
-        </Box>
-      </Flex>
-    </Box>
+          <Flex>
+            <Text color={'gray.500'} fontSize={'xs'} fontStyle={'italic'}>
+              {department ? department : ''}
+              {subcategory ? ', ' + subcategory : ''}
+              {building_type ? ', ' + building_type : ''}
+            </Text>
+          </Flex>
+          <Box>
+            <Heading as='h4' size={'md'}>
+              {name}
+            </Heading>
+          </Box>
+          <Box marginBottom={'20px'}>
+            <Text noOfLines={3} lineHeight='short'>
+              {description ? description : ''}
+            </Text>
+          </Box>
+          <Box>
+            {link ? (
+              <HStack spacing={'4'}>
+                <Tag
+                  size={'md'}
+                  key={'md'}
+                  variant='solid'
+                  color={'white'}
+                  bg={'blackAlpha.800'}
+                  py={'6px'}
+                  px={'12px'}
+                  borderRadius='4px'
+                  css={{ cursor: 'pointer' }}
+                >
+                  <TagLabel fontSize={'sm'}>
+                    <a href={`${link}`} target='_blank' rel='noreferrer'>
+                      View Project
+                    </a>
+                  </TagLabel>
+                </Tag>
+              </HStack>
+            ) : (
+              ''
+            )}
+          </Box>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 }
 
